ec4c7c66d1d759f3f112edd1539068a85827df27
LanceXianyiLiang <lance.liang@auroraquestinc.com>
2025-12-19 21:03:07 -0500

    feat: add success stories

---
+++ src/data/offers.ts
@@ -0,0 +1,108 @@
+export interface Offer {
+  id: number;
+  image: string;
+  alt: string;
+}
+
+export const offers: Offer[] = [
+  {
+    id: 1,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0001.jpg",
+    alt: "Student Offer 1",
+  },
+  {
+    id: 2,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0002.jpg",
+    alt: "Student Offer 2",
+  },
+  {
+    id: 3,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0003.jpg",
+    alt: "Student Offer 3",
+  },
+  {
+    id: 4,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0004.jpg",
+    alt: "Student Offer 4",
+  },
+  {
+    id: 5,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0005.jpg",
+    alt: "Student Offer 5",
+  },
+  {
+    id: 6,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0006.jpg",
+    alt: "Student Offer 6",
+  },
+  {
+    id: 7,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0007.jpg",
+    alt: "Student Offer 7",
+  },
+  {
+    id: 8,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0008.jpg",
+    alt: "Student Offer 8",
+  },
+  {
+    id: 9,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0009.jpg",
+    alt: "Student Offer 9",
+  },
+  {
+    id: 10,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0010.jpg",
+    alt: "Student Offer 10",
+  },
+  {
+    id: 11,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0011.jpg",
+    alt: "Student Offer 11",
+  },
+  {
+    id: 12,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0012.jpg",
+    alt: "Student Offer 12",
+  },
+  {
+    id: 13,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0013.jpg",
+    alt: "Student Offer 13",
+  },
+  {
+    id: 14,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0014.jpg",
+    alt: "Student Offer 14",
+  },
+  {
+    id: 15,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0015.jpg",
+    alt: "Student Offer 15",
+  },
+  {
+    id: 16,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0016.jpg",
+    alt: "Student Offer 16",
+  },
+  {
+    id: 17,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0017.jpg",
+    alt: "Student Offer 17",
+  },
+  {
+    id: 18,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0018.jpg",
+    alt: "Student Offer 18",
+  },
+  {
+    id: 19,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0019.jpg",
+    alt: "Student Offer 19",
+  },
+  {
+    id: 20,
+    image: "/images/offers/2.录取通知书展示(部分)_副本_page-0020.jpg",
+    alt: "Student Offer 20",
+  },
+];
